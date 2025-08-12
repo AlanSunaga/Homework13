04dfbe2cdf141836180c8af116dd8f9aefc0ae25
AlanSunaga <Shumvavi@mail.ru>
2025-08-12 18:31:13 +0300

    Сделал

--- src/org/skypro/skyshop/basket/ProductBasket.java
+++ src/org/skypro/skyshop/basket/ProductBasket.java
@@ -3,124 +3,63 @@ package org.skypro.skyshop.basket;
 import org.skypro.skyshop.product.Product;
 
 import java.util.*;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 
 public class ProductBasket {
-    private Map<String, Product> products = new HashMap<>();
+    private Map<String, List<Product>> products = new HashMap<>();
 
     private int sum;
     private int scoreIsSpecial = 0;
-    private boolean w = true;
-
+    private boolean productExist = true;
 
     public void addProductName(Product product) {
-        products.put(product.getNameProduct(), product);
+        products.computeIfAbsent(product.getNameProduct(), k -> new ArrayList<>()).add(product);
+
     }
 
-    public int getTotalPrice() {
+    public double getTotalPrice() {
 
-        for (String name : products.keySet()) {
-            if (products.get(name) == null) {
-                return 0;
-            } else {
-                for (int i = 0; i < products.size(); i++) {
-                    sum += products.get(name).getPriceProduct();
-                }
-            }
-        }
-
-        return sum;
+        return products.values().stream().flatMap(Collection::stream)
+                .mapToDouble(Product::getPriceProduct)
+                .sum();
     }
 
-    public boolean searchNull() {
-        for (Product name : products.values()) {
-            if (name == null) {
-                w = false;
-            }
-        }
-        return w;
-    }
-
-    public void nameString() throws IllegalAccessException {
-
-
-        if (searchNull()) {
-            for (Product product : products.values()) {
-                if (product == null) {
-                    throw new IllegalAccessException();
-                } else {
-                    System.out.println(product);
-                    sum += product.getPriceProduct();
-                    if (product.isSpecial()) {
-                        scoreIsSpecial++;
-                    }
-                }
-
-            }
-            System.out.println("Итого: " + sum);
-            System.out.println("Специальных товаров: " + scoreIsSpecial);
-        } else System.out.println("Корзина пуста");
-
+    public boolean productEx() {
+        Stream<Product> basket = products.values().stream().flatMap(Collection::stream)
+                .filter(Objects::nonNull);
+        return productExist;
     }
 
     public void printBacket() throws IllegalAccessException {
-
-        if (w) {
-            for (Product product : products.values()) {
-                if (product == null) {
-                    throw new IllegalAccessException();
-                } else {
-                    System.out.println(product);
-                }
-
-            }
+        double sum = getTotalPrice();
+        Stream<Product> basket = products.values().stream().flatMap(Collection::stream);
+        if (sum == 0) {
+            System.out.println("в корзине пусто");
+        } else {
+            System.out.println(basket.toList());
             System.out.println("Итого: " + sum);
             System.out.println("Специальных товаров: " + scoreIsSpecial);
-        } else System.out.println("Корзина пуста");
+        }
     }
 
     public Boolean searchProduct(String name) throws IllegalAccessException {
-
-        if (!searchNull()) {
-            return false;
-        } else {
-            for (Product product : products.values()) {
-                if (product == null) {
-                    throw new IllegalAccessException();
-                } else if (product.getNameProduct() == name) {
-                    return true;
-                }
-            }
-            return false;
+        try {
+            return products.values().stream().flatMap(Collection::stream)
+                    .allMatch(table -> table.getNameProduct().toLowerCase().contains(name.toLowerCase()));
+        } catch (RuntimeException e) {
+            throw new IllegalAccessException();
         }
     }
 
-
     public List<Product> deletingAProductByName(String name) {
-        List<Product> delete = new ArrayList<>();
-        for (Product product : products.values()) {
-            if (product == null) {
-                if (product.getNameProduct() == null) {
-                    for (String productKey : products.keySet()) {
-                        if (productKey.contains(product.getNameProduct())) {
-                            products.remove(name);
-                        }
-                    }
-                } else if (name.equals(product.getNameProduct())) {
-                    delete.add(product);
-                    System.out.println('\n' + "Список удаленных продуктов");
-                    return delete;
-                }
-            }
-        }
-        System.out.println('\n' + "Список удаленных продуктов пуст" + '\n');
-        return delete;
+        return products.remove(name);
     }
 
     public void cleanBascket() {
-        for (String productKey : products.keySet()) {
-            products.remove(productKey);
-        }
+        products.clear();
 
     }
 
